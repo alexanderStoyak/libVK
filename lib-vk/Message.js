80124da874c3653aa0c57159afa3eb4123e9aca9
Stoyak <97674215+alexanderStoyak@users.noreply.github.com>
2022-01-17 16:52:02 +0300

    new

---
+++ lib-vk/Message.js
@@ -0,0 +1,28 @@
+const structMessage = require('./utils/messageStruct');
+
+module.exports = 
+class Message
+{
+    constructor (message) 
+    {
+        const typeMessage = structMessage[message[0]] ? structMessage[message[0]].type : '';
+        if(typeMessage === 'messageNew' || typeMessage === 'messageEdit') 
+        {
+            structMessage[message[0]].struct.forEach((key, index) => this[key] = message[index])
+            this.text = this.text ? this.text.replace(/<br>/g, '\n') : this.text;
+            (!this.peer_id > 2e9) && (this.conversation_message_id = 0)
+        }
+        else if (typeMessage === 'userOnline' || typeMessage === 'userOffline') 
+        {
+            structMessage[message[0]].struct.forEach((key, index) => this[key] = message[index]);
+            this.userId = this.userId * -1;
+        }
+        else
+        {
+            structMessage[message[0]].struct.forEach((key, index) => this[key] = message[index+1]);
+        }
+
+
+        this.type = typeMessage;
+    }
+}
---
+++ lib-vk/utils/messageStruct.js
@@ -0,0 +1,104 @@
+module.exports = 
+{
+    4: {
+        // Adding a new message.
+        type: 'messageNew',
+        struct: [
+            'type',
+            'id',
+            'conversation_message_id',
+            'peer_id',
+            'date',
+            'text'
+        ]
+    },
+    5: {
+        // Edit the message.
+        type: 'messageEdit',
+        struct: [
+            'type',
+            'id',
+            'conversation_message_id',
+            'peer_id',
+            'date',
+            'text'
+        ]
+    },
+    7: {
+        // Reading all outgoing messages in $peerId that arrived before the message with $localId
+        type: 'ReadingAllOutMessages',
+        struct: [
+            'peerId',
+            'localId',
+
+        ]
+    },
+    /** A friend of $userId has become online. The extra contains the platform ID.
+    *    $timestamp — the time of the last action of the user $userId on the site.
+    */
+    8: {
+        type: 'userOnline',
+        struct: [
+            'type',
+            'userId',
+            'extra',
+            'timestamp'
+        ]
+    },
+    /** Friend $userId has become offline ($flags is 0 if the user has left the site and 1 if offline by timeout )
+    *    $timestamp — the time of the last action of the user $userId on the site.
+    */
+    9: {
+        type: 'userOffline',
+        struct: [
+            'type',
+            'userId',
+            'flags',
+            'timestamp'
+        ]
+    },
+    /** The user $userId is typing text in the dialog.
+    *    The event comes once every ~5 seconds when typing. $flags = 1. 
+    */
+    61: {
+        type: 'messageTyping',
+        struct: [
+            'userId',
+            'flags'
+    
+        ]
+    },
+    /** The user $userId types text in the conversation $chatId. 
+    */
+    62: {
+        type: 'messageTypingIsChat',
+        struct: [
+            'userId',
+            'chatId' 
+        ]
+    },
+    /** Users $userIds type text in the conversation $peerId.
+    *   A maximum of five conversation participants are transmitted, the total number of printers is indicated in $totalCount.
+    *   $ts is the time when this event was generated.
+    */
+    63: {
+        type: 'messageTypingsIsChat',
+        struct: [
+            'userIds',
+            'peerId',
+            'totalCount',
+            'ts'
+        ]
+    },
+    /** Users $userIds record an audio message in the conversation $peerId.
+    */
+    64: {
+        type: 'recordsAudiomessage',
+        struct: [
+            'userIds',
+            'peerId',
+            'totalCount',
+            'ts'
+        ]
+    }
+}
